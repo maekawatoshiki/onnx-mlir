5bdd3797c329ce3c7f8a33480d0c2d3aed86cef7
maekawatoshiki <maekawatoshiki1017@gmail.com>
2025-06-16 23:40:50 +0900

    Apply shellcheck on build.sh

--- build.sh
+++ build.sh
@@ -18,7 +18,7 @@ if [ ! -d llvm-project/build ]; then
     )
 fi
 
-export pythonLocation=$(which python3)
+PYTHON=$(which python3)
 if [ ! -d env ]; then
     python3 -m venv env
     source env/bin/activate
@@ -33,23 +33,23 @@ if [ ! -d protobuf ]; then
     (
     git clone -b v${PROTOBUF_VERSION} --recursive https://github.com/protocolbuffers/protobuf.git \
         && cd protobuf && ./autogen.sh \
-        && CC='ccache gcc' CXX='ccache g++' ./configure --enable-static=no --prefix=$PWD/target \
+        && CC='ccache gcc' CXX='ccache g++' ./configure --enable-static=no --prefix="${PWD}/target" \
         && make -j install \
         && cd python && python3 setup.py install --cpp_implementation
     )
 fi
 
-MLIR_DIR=$(pwd)/llvm-project/build/lib/cmake/mlir
-export LD_LIBRARY_PATH=$(pwd)/protobuf/target/lib:$LD_LIBRARY_PATH
+MLIR_DIR="${PWD}/llvm-project/build/lib/cmake/mlir"
+export LD_LIBRARY_PATH="${PWD}/protobuf/target/lib:${LD_LIBRARY_PATH}"
 (
 mkdir -p build
 cd build
 cmake -G Ninja .. \
     -DCMAKE_BUILD_TYPE=Release \
-    -DCMAKE_PREFIX_PATH="$PWD/../protobuf/target;$PWD/../protobuf/cmake" \
+    -DCMAKE_PREFIX_PATH="${PWD}/../protobuf/target;${PWD}/../protobuf/cmake" \
     -DCMAKE_CXX_COMPILER=/usr/lib/ccache/g++ \
-    -DPython3_ROOT_DIR=$pythonLocation \
-    -DMLIR_DIR=${MLIR_DIR} \
+    -DPython3_ROOT_DIR="${PYTHON}" \
+    -DMLIR_DIR="${MLIR_DIR}" \
     -DCMAKE_CXX_FLAGS=-march=native
 cmake --build .
 
